d3a04001b28f11ba3dfd6021cceae8204437038e
mehdiborji <borjijiji@gmail.com>
2024-04-16 19:49:05 -0400

    add new script to do both alignment and methylation extraction in one job. this results in fewer jobs but waste of requested resources since extraction doesn't need as much as memory or cores

---
+++ scripts/bismark_align_methex.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -c 3
+#SBATCH --mem=1G
+#SBATCH -t 0:30:00
+#SBATCH -p short
+#SBATCH -o methylation_extractor_job_%A.out
+
+read1=$1
+read2=$2
+out_dir=$3
+ref_dir=$4
+bam=$5
+
+module load gcc/9.2.0
+module load bowtie2/2.5.1
+
+
+/home/meb521/Bismark-0.24.2/bismark --pbat --score_min L,0,-.6 $ref_dir -1 $read1 -2 $read2 -o $out_dir
+
+/home/meb521/Bismark-0.24.2/bismark_methylation_extractor $bam \
+--comprehensive --merge_non_CpG --gzip --buffer_size 1G \
+--include_overlap --paired-end --no_header --output_dir $out_dir
