fed5ea0e0ec81f0d4efe2d6d711b04dcbbb75e1d
mehdiborji <borjijiji@gmail.com>
2024-04-16 20:16:42 -0400

    add barcode-aware samtools markdup command for. barcode TAG is hardcoded as BC. needs samtools at least 1.16 probably higher due to bugs

--- scripts/bam_merge.sh
+++ scripts/bam_merge.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #SBATCH -c 20
-#SBATCH --mem=60G
-#SBATCH -t 2:30:00
+#SBATCH --mem=10G
+#SBATCH -t 0:20:00
 #SBATCH -p priority
 #SBATCH -o merge_bam_job_%A.out
 
-samtools merge -@20 -n -f -o $1/$2_name_sorted.bam $1/split/*/*.bam
+#samtools merge -@20 -n -f -o $1/$2/$2_name_sorted.bam $1/$2/split/output_*/*_tagged.bam
+#samtools fixmate -@20 -m $1/$2/$2_name_sorted.bam $1/$2/$2_name_sorted_fixmate.bam
+#samtools sort -@20 -o $1/$2/$2_pos_sorted.bam $1/$2/$2_name_sorted_fixmate.bam
+#samtools index -@20 $1/$2/$2_pos_sorted.bam
 
-samtools sort -@20 -o $1/$2_pos_sorted.bam $1/$2_name_sorted.bam
-samtools index -@20 $1/$2_pos_sorted.bam
+samtools markdup -@20 --json --barcode-tag BC $1/$2/$2_pos_sorted.bam $1/$2/$2_markdup.bam
 
 #samtools sort -@20 -o $1/$2_pos_sorted.deduplicated.bam $1/$2_name_sorted.deduplicated.bam
 #samtools index -@20 $1/$2_pos_sorted.deduplicated.bam
