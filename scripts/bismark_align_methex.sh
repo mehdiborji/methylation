0117de5bd6cbe4b1d58f93aae17939b1bef7d6e6
mehdiborji <borjijiji@gmail.com>
2024-04-17 16:39:30 -0400

    lenient mapping, 1 hour, 10GB, %50 core usage for 2.8m input reads and %80 mapping

--- scripts/bismark_align_methex.sh
+++ scripts/bismark_align_methex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH -c 3
-#SBATCH --mem=1G
-#SBATCH -t 0:30:00
+#SBATCH -c 4
+#SBATCH --mem=20G
+#SBATCH -t 1:30:00
 #SBATCH -p short
 #SBATCH -o methylation_extractor_job_%A.out
 
@@ -15,8 +15,7 @@ bam=$5
 module load gcc/9.2.0
 module load bowtie2/2.5.1
 
-
-/home/meb521/Bismark-0.24.2/bismark --pbat --score_min L,0,-.6 $ref_dir -1 $read1 -2 $read2 -o $out_dir
+/home/meb521/Bismark-0.24.2/bismark --pbat --score_min L,0,-1 $ref_dir -1 $read1 -2 $read2 -o $out_dir
 
 /home/meb521/Bismark-0.24.2/bismark_methylation_extractor $bam \
 --comprehensive --merge_non_CpG --gzip --buffer_size 1G \
