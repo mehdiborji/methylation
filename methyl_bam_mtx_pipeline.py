5c4e873d8c79b6f4f458f06df792af47f41dfc3d
mehdiborji <borjijiji@gmail.com>
2024-05-14 22:19:19 -0400

    minor ruff format and organize

--- methyl_bam_mtx_pipeline.py
+++ methyl_bam_mtx_pipeline.py
@@ -1,16 +1,14 @@
 import argparse
 from multiprocessing import Pool
 import methyl_utils
-import os
 import pandas as pd
-import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--cores', type=str)
-parser.add_argument('-i', '--indir', type=str)
-parser.add_argument('-s', '--sample', type=str)
-parser.add_argument('-w', '--window_size', type=int)
-parser.add_argument('-l', '--limit', default=False, action='store_true')
+parser.add_argument("-c", "--cores", type=str)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-w", "--window_size", type=int)
+parser.add_argument("-l", "--limit", default=False, action="store_true")
 args = parser.parse_args()
 
 cores = args.cores
@@ -21,10 +19,7 @@ limit = args.limit
 
 ######################################################
 
-
-######################################################
-
-parts = methyl_utils.find_sub_fastq_parts(indir,sample)
+parts = methyl_utils.find_sub_fastq_parts(indir, sample)
 args = [(indir, sample, part, limit) for part in parts]
 """
 pool = Pool(int(cores))
@@ -45,27 +40,31 @@ pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.save_quad_batch_json, args)
 pool.close()
 pool.join()
-
-bcs = pd.read_csv(f'{indir}/{sample}/{sample}_whitelist.csv')
-#sub_batch_N = int(np.sqrt(len(bcs)))+1
+######################################################
+bcs = pd.read_csv(f"{indir}/{sample}/{sample}_whitelist.csv")
+# sub_batch_N = int(np.sqrt(len(bcs)))+1
 sub_batch_N = 20
-args = [(indir, sample, str(j+1).zfill(3)) for j in range(sub_batch_N)]
+args = [(indir, sample, str(j + 1).zfill(3)) for j in range(sub_batch_N)]
 
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.aggregate_quad_parts, args)
 pool.close()
 pool.join()
-
+######################################################
 chr_idx_dict = methyl_utils.chrom_to_windows(methyl_utils.hg38_chroms, window_size)
 
-print('chr_idx_dict lenght = ',len(chr_idx_dict))
+print("chr_idx_dict lenght = ", len(chr_idx_dict))
 
-#bcs = pd.read_csv(f'{indir}/{sample}/{sample}_whitelist.csv')
-#sub_batch_N = int(np.sqrt(len(bcs)))+1
-args = [(indir, sample, str(j+1).zfill(3), window_size, chr_idx_dict) for j in range(sub_batch_N)]
+# bcs = pd.read_csv(f'{indir}/{sample}/{sample}_whitelist.csv')
+# sub_batch_N = int(np.sqrt(len(bcs)))+1
+args = [
+    (indir, sample, str(j + 1).zfill(3), window_size, chr_idx_dict)
+    for j in range(sub_batch_N)
+]
 
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.make_count_sparse_mtx_batch_windows, args)
 pool.close()
 pool.join()
+######################################################
 """"""
