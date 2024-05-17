70dc2de83ec047136d4be7a6d0beefe1eb593ec0
mehdiborji <borjijiji@gmail.com>
2024-05-17 17:58:32 -0400

    move script

--- methyl_alignment_pipeline.py
+++ methyl_alignment_pipeline.py
@@ -1,31 +1,33 @@
 import argparse
 import methyl_utils
 import os
+import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--cores', type=str)
 parser.add_argument('-i', '--indir', type=str)
 parser.add_argument('-s', '--sample', type=str)
 parser.add_argument('-r', '--ref_dir', type=str)
 parser.add_argument('-j', '--job_submit', default=False, action='store_true')
+parser.add_argument('-b', '--sbatch', default=False, action='store_true')
 args = parser.parse_args()
 
-cores = args.cores
 indir = args.indir
 sample = args.sample
 ref_dir = args.ref_dir
 job_submit = args.job_submit
+sbatch = args.sbatch
 
 ######################################################
 
 parts = methyl_utils.find_sub_fastq_parts(indir,sample)
-args = [(indir, sample, part, limit) for part in parts]
 
+print('current dir:', os.getcwd())
 os.chdir(indir)
-
+print('current dir:', os.getcwd())
 # ref_dir = "/n/scratch/users/m/meb521/GRCh38_v44/"
 
 command = "/home/meb521/methylation/scripts/bismark_align_methex.sh"
+
 for p in parts:
     fq1 = f"{sample}/split/{sample}_R1.part_{p}_clean.fastq"
     fq2 = f"{sample}/split/{sample}_R3.part_{p}_clean.fastq"
@@ -37,7 +39,10 @@ for p in parts:
     
     mbias_exists = os.path.isfile(mbias)
     if not mbias_exists:
-        submit = f"sbatch {command} {fq1} {fq2} {outdir} {ref_dir} {bam}"
+        if sbatch:
+            submit = f"sbatch {command} {fq1} {fq2} {outdir} {ref_dir} {bam}"
+        else:
+            submit = f"{command} {fq1} {fq2} {outdir} {ref_dir} {bam}"
         print(submit)
         if job_submit:
             subprocess.call(submit, shell=True)
