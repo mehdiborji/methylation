7486e9daac08c22d8e4a3d345fa625a9d8945d69
mehdiborji <borjijiji@gmail.com>
2024-05-17 11:34:31 -0400

    ruff format and minor bug fix

--- methyl_fastq_pipeline.py
+++ methyl_fastq_pipeline.py
@@ -34,47 +34,68 @@ pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.extract_clean_fastq, args)
 pool.close()
 pool.join()
-######################################################
-
-methyl_utils.aggregate_bc_dicts(indir,sample)
-
-methyl_utils.write_bc_raw_reads(indir, sample, 20000)
 
 ######################################################
+
+methyl_utils.aggregate_bc_dicts(indir, sample)
+
+methyl_utils.write_bc_raw_reads(indir, sample, 2)
+
+######################################################
+
 if barcodes is None:
-    whitelist = f'{py_dir}/data/737K-arc-v1.txt.gz'
+    whitelist = f"{py_dir}/data/737K-arc-v1.txt.gz"
 
 methyl_utils.write_bc_whitelist(indir, sample, whitelist)
 
 ######################################################
 
-ref_generation_log = f'{indir}/{sample}/{sample}_ref/Log.out'
+ref_generation_log = f"{indir}/{sample}/{sample}_ref/Log.out"
 
 if os.path.isfile(ref_generation_log):
-    print('ref generation log', ref_generation_log,' exists')
-    with open(ref_generation_log, 'r') as file:
+    print("ref generation log", ref_generation_log, " exists")
+    with open(ref_generation_log, "r") as file:
         lines = file.readlines()
         last_line = lines[-1].strip()
     print(last_line)
-    if last_line != 'DONE: Genome generation, EXITING':
-        print('last line of Genome generation log is not what it should be, possibly corrupt reference')
+    if last_line != "DONE: Genome generation, EXITING":
+        print(
+            "last line of Genome generation log is not what it should be, possibly corrupt reference"
+        )
 else:
-    print('matching file,', sam_file,' does not exist, will make reference')
-    subprocess.call([ f'{py_dir}/scripts/barcode_ref.sh', f'{indir}/{sample}/{sample}_bc_whitelist.fasta', f'{indir}/{sample}/{sample}_ref/'])
+    print(
+        "ref generation log", ref_generation_log, " does not exist, will make reference"
+    )
+    subprocess.call(
+        [
+            f"{py_dir}/scripts/barcode_ref.sh",
+            f"{indir}/{sample}/{sample}_bc_whitelist.fasta",
+            f"{indir}/{sample}/{sample}_ref/",
+        ]
+    )
 
 ######################################################
 
-alignment_log = f'{indir}/{sample}/{sample}_matchingLog.final.out'
+alignment_log = f"{indir}/{sample}/{sample}_matchingLog.final.out"
 
 if os.path.isfile(alignment_log):
-    print('alignment log', alignment_log,' exists')
+    print("alignment log", alignment_log, " exists")
 else:
-    print('alignment log', alignment_log,' does not exist, will perform alignment')
-    subprocess.call([ f'{py_dir}/scripts/barcode_align.sh', f'{indir}/{sample}/{sample}_bc_raw_reads.fasta', f'{indir}/{sample}/{sample}_ref/', f'{indir}/{sample}/{sample}_matching', cores, '-1'])
+    print("alignment log", alignment_log, " does not exist, will perform alignment")
+    subprocess.call(
+        [
+            f"{py_dir}/scripts/barcode_align.sh",
+            f"{indir}/{sample}/{sample}_bc_raw_reads.fasta",
+            f"{indir}/{sample}/{sample}_ref/",
+            f"{indir}/{sample}/{sample}_matching",
+            cores,
+            "-1",
+        ]
+    )
 
 ######################################################
-    
-    
+
+
 """
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.extract_bc_from_bam, args)
