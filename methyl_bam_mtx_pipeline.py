6c16837af48e5e0defe99d1243190005f56e0a35
mehdiborji <borjijiji@gmail.com>
2024-05-20 18:05:21 -0400

    update to accept methylation_contexts and reference_genome_index as input argument

--- methyl_bam_mtx_pipeline.py
+++ methyl_bam_mtx_pipeline.py
@@ -8,6 +8,8 @@ parser.add_argument("-c", "--cores", type=str)
 parser.add_argument("-i", "--indir", type=str)
 parser.add_argument("-s", "--sample", type=str)
 parser.add_argument("-w", "--window_size", type=int)
+parser.add_argument("-m", "--methylation_context", type=str)
+parser.add_argument("-r", "--reference_genome_index", type=str)
 parser.add_argument("-l", "--limit", default=False, action="store_true")
 args = parser.parse_args()
 
@@ -15,12 +17,13 @@ cores = args.cores
 indir = args.indir
 sample = args.sample
 window_size = args.window_size
+methylation_context = args.methylation_context
+reference_genome_index = args.reference_genome_index
 limit = args.limit
 
 ######################################################
 
 parts = methyl_utils.find_sub_fastq_parts(indir, sample)
-args = [(indir, sample, part, limit) for part in parts]
 """
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.tag_bam_with_barcodes, args)
@@ -36,29 +39,27 @@ pool.join()
 methyl_utils.aggregate_bc_dicts(indir,sample)
 """
 
+args = [(indir, sample, part, methylation_context, limit) for part in parts]
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.save_quad_batch_json, args)
 pool.close()
 pool.join()
 ######################################################
-bcs = pd.read_csv(f"{indir}/{sample}/{sample}_whitelist.csv")
-# sub_batch_N = int(np.sqrt(len(bcs)))+1
 sub_batch_N = 20
-args = [(indir, sample, str(j + 1).zfill(3)) for j in range(sub_batch_N)]
+args = [(indir, sample, str(j+1).zfill(3), methylation_context) for j in range(sub_batch_N)]
 
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.aggregate_quad_parts, args)
 pool.close()
 pool.join()
 ######################################################
-chr_idx_dict = methyl_utils.chrom_to_windows(methyl_utils.hg38_chroms, window_size)
+
+chr_idx_dict = methyl_utils.fasta_index_to_windows(reference_genome_index, window_size)
 
 print("chr_idx_dict lenght = ", len(chr_idx_dict))
 
-# bcs = pd.read_csv(f'{indir}/{sample}/{sample}_whitelist.csv')
-# sub_batch_N = int(np.sqrt(len(bcs)))+1
 args = [
-    (indir, sample, str(j + 1).zfill(3), window_size, chr_idx_dict)
+    (indir, sample, str(j + 1).zfill(3), window_size, chr_idx_dict, methylation_context)
     for j in range(sub_batch_N)
 ]
 
