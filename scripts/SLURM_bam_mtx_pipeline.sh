3aa54f0513405449544f781e140a98291f024531
mehdiborji <borjijiji@gmail.com>
2024-05-22 23:52:53 -0400

    5GB memory used for each 160 cell batch of mCH data for 30k reads per cell

--- scripts/SLURM_bam_mtx_pipeline.sh
+++ scripts/SLURM_bam_mtx_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -c 20
-#SBATCH --mem=80G
-#SBATCH -t 0:30:00
+#SBATCH -c 5
+#SBATCH --mem=32G
+#SBATCH -t 1:20:00
 #SBATCH -p short
 #SBATCH -o methyl_bam_mtx_job_%A.out
 #SBATCH --account=chen_fec176
@@ -12,5 +12,5 @@ echo 'window =' $3
 echo 'context =' $4
 echo 'reference_index =' $5
 
-python ~/methylation/methyl_bam_mtx_pipeline.py -c 20 -i $1 -s $2 -w $3 -m $4 -r $5
+python ~/methylation/methyl_bam_mtx_pipeline.py -c 5 -i $1 -s $2 -w $3 -m $4 -r $5
 #--limit
