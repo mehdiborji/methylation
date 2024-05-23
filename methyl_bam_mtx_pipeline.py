7d3a318bb2e5e568dcf2d3fd519b4ebad1eaeaad
mehdiborji <borjijiji@gmail.com>
2024-05-22 23:50:08 -0400

    add bam tagging step to the end of the pipeline

--- methyl_bam_mtx_pipeline.py
+++ methyl_bam_mtx_pipeline.py
@@ -1,7 +1,6 @@
 import argparse
 from multiprocessing import Pool
 import methyl_utils
-import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--cores", type=str)
@@ -21,37 +20,29 @@ methylation_context = args.methylation_context
 reference_genome_index = args.reference_genome_index
 limit = args.limit
 
+sub_batch_N = 20
+
 ######################################################
 
 parts = methyl_utils.find_sub_fastq_parts(indir, sample)
 """
-pool = Pool(int(cores))
-results = pool.starmap(methyl_utils.tag_bam_with_barcodes, args)
-pool.close()
-pool.join()
-
-
-pool = Pool(int(cores))
-results = pool.starmap(methyl_utils.extract_bc_from_bam, args)
-pool.close()
-pool.join()
-
-methyl_utils.aggregate_bc_dicts(indir,sample)
-"""
+######################################################
 
 args = [(indir, sample, part, methylation_context, limit) for part in parts]
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.save_quad_batch_json, args)
 pool.close()
 pool.join()
+
 ######################################################
-sub_batch_N = 20
+
 args = [(indir, sample, str(j+1).zfill(3), methylation_context) for j in range(sub_batch_N)]
 
 pool = Pool(int(cores))
 results = pool.starmap(methyl_utils.aggregate_quad_parts, args)
 pool.close()
 pool.join()
+
 ######################################################
 
 chr_idx_dict = methyl_utils.fasta_index_to_windows(reference_genome_index, window_size)
@@ -68,4 +59,13 @@ results = pool.starmap(methyl_utils.make_count_sparse_mtx_batch_windows, args)
 pool.close()
 pool.join()
 ######################################################
-""""""
+"""
+
+parts = methyl_utils.find_sub_fastq_parts(indir, sample)
+args = [(indir, sample, part, limit) for part in parts]
+pool = Pool(int(cores))
+results = pool.starmap(methyl_utils.tag_bam_with_barcodes, args)
+pool.close()
+pool.join()
+
+######################################################
