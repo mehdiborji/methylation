80a06c5afb8c45f5e9c9601cc09d44ebdbdc53bd
mehdiborji <borjijiji@gmail.com>
2024-06-13 23:32:11 -0400

    tweak slurm jobs

--- scripts/SLURM_fastq_pipeline.sh
+++ scripts/SLURM_fastq_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -c 12
+#SBATCH -c 20
 #SBATCH --mem=6G
-#SBATCH -t 0:20:00
+#SBATCH -t 1:20:00
 #SBATCH -p priority
 #SBATCH -o methyl_fastq_job_%A.out
 #SBATCH --account=chen_fec176
@@ -9,5 +9,5 @@
 echo 'indir =' $1
 echo 'sample =' $2
 
-python ~/methylation/methyl_fastq_pipeline.py -c 12 -i $1 -s $2
+python ~/methylation/methyl_fastq_pipeline.py -c 20 -i $1 -s $2
 #--limit
