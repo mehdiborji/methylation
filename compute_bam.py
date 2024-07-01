78a01252544c3979ed79bded7fcb73f9ed622369
mehdiborji <borjijiji@gmail.com>
2024-07-01 09:54:19 -0400

    add stand alone scripts for processing final bam
    computes unique/duplicate fragments per chormosome
    
    other stats such as fragments sizes or genomic bins for each barcode can be added

---
+++ compute_bam.py
@@ -0,0 +1,28 @@
+import argparse
+from multiprocessing import Pool
+import methyl_utils
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cores", type=str)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-r", "--reference_genome_index", type=str)
+parser.add_argument("-l", "--limit", default=False, action="store_true")
+args = parser.parse_args()
+
+cores = args.cores
+indir = args.indir
+sample = args.sample
+reference_genome_index = args.reference_genome_index
+limit = args.limit
+
+chrs = pd.read_table(reference_genome_index, header=None)
+
+chrs = chrs[chrs[0].str.startswith('chr')][0].tolist()
+
+args = [(indir, sample, c, limit) for c in chrs]
+pool = Pool(int(cores))
+results = pool.starmap(methyl_utils.compute_dup_rate, args)
+pool.close()
+pool.join()
---
+++ scripts/SLURM_compute_bam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -c 20
+#SBATCH --mem=4G
+#SBATCH -t 0:50:00
+#SBATCH -p short
+#SBATCH -o compute_bam_job_%A.out
+#SBATCH --account=chen_fec176
+
+echo 'indir =' $1
+echo 'sample =' $2
+echo 'reference_index =' $3
+
+python ~/methylation/compute_bam.py -c 20 -i $1 -s $2 -r $3
+#--limit
