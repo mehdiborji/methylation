b301c87d1869da818e61f2c8bfc7a7c23760d4bc
mehdiborji <borjijiji@gmail.com>
2024-07-02 14:14:54 -0400

    stand alone method to run aggregation as array job

---
+++ aggregate_quad_parts.py
@@ -0,0 +1,25 @@
+import argparse
+from multiprocessing import Pool
+import methyl_utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cores", type=int)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-m", "--methylation_context", type=str)
+parser.add_argument("-p", "--parts_batch", type=int)
+args = parser.parse_args()
+
+cores = args.cores
+indir = args.indir
+sample = args.sample
+methylation_context = args.methylation_context
+parts_batch = args.parts_batch
+
+start = (parts_batch - 1) * 5
+end = parts_batch * 5
+
+for b in range(start,end):
+    batch = str(b + 1).zfill(3)
+    print(batch)
+    methyl_utils.aggregate_quad_parts(indir, sample, batch, methylation_context)
