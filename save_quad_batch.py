79574a6eb32c4716ce2b570b5f1a270827eafd86
mehdiborji <borjijiji@gmail.com>
2024-07-22 00:29:10 -0400

    add python script to run function save_quad_batch_from_bam based on input arguments
    each function runs 12 of the bam parts, can be modified within the script

---
+++ save_quad_batch.py
@@ -0,0 +1,29 @@
+import argparse
+from multiprocessing import Pool
+import methyl_utils
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cores", type=int)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-l", "--limit", default=False, action="store_true")
+parser.add_argument("-p", "--parts_batch", type=int)
+args = parser.parse_args()
+
+cores = args.cores
+indir = args.indir
+sample = args.sample
+limit = args.limit
+parts_batch = args.parts_batch
+
+
+parts = methyl_utils.find_sub_fastq_parts(indir, sample)
+
+start = (parts_batch - 1) * 12
+end = parts_batch * 12
+
+args = [(indir, sample, part, limit) for part in parts[start:end]]
+pool = Pool(cores)
+results = pool.starmap(methyl_utils.save_quad_batch_from_bam, args)
+pool.close()
+pool.join()
