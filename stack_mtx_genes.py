7b67c26ef9a0077ba49efb948a84ca11986a6bf8
mehdiborji <borjijiji@gmail.com>
2024-08-01 19:34:26 -0400

    add wrapper for stacking gene level mtx

---
+++ stack_mtx_genes.py
@@ -0,0 +1,24 @@
+import argparse
+from multiprocessing import Pool
+import methyl_utils
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cores", type=int)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-m", "--methylation_context", type=str)
+parser.add_argument("-r", "--reference_gencode", type=str)
+
+args = parser.parse_args()
+
+cores = args.cores
+indir = args.indir
+sample = args.sample
+context = args.methylation_context
+reference_gencode = args.reference_gencode
+
+df_gtf_genes = pd.read_csv(reference_gencode)
+gene_idx_list = df_gtf_genes['gene_id'].tolist()
+    
+methyl_utils.stack_mtx_genes(indir, sample, gene_idx_list, context, cores)
