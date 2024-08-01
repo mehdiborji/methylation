9268468497c489ef8a9a1c37ab0656d28eccb408
mehdiborji <borjijiji@gmail.com>
2024-07-31 21:34:53 -0400

    rename file, minor changes

---
+++ scripts/SLURM_make_count_mtx_windows.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --mem=8G
+#SBATCH -t 0:30:00
+#SBATCH -p short
+#SBATCH -o make_count_mtx_windows_job_%A_%a.out
+#SBATCH --array=1-10
+#SBATCH --account=chen_fec176
+
+echo 'indir =' $1
+echo 'sample =' $2
+echo 'window =' $3
+echo 'methylation_context =' $4
+echo 'reference_index =' $5
+
+python ~/methylation/make_count_mtx_windows.py -i $1 -s $2 -w $3 -m $4 -r $5 -p ${SLURM_ARRAY_TASK_ID}
