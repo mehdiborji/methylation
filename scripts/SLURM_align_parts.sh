5282b2a71d2d5fe7fbb6e0881c0f4e76f9c8495d
mehdiborji <borjijiji@gmail.com>
2024-08-22 23:48:17 -0400

    array job script for alignment

---
+++ scripts/SLURM_align_parts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -c 5
+#SBATCH --mem=17G
+#SBATCH -t 6:30:00
+#SBATCH -p short
+#SBATCH -o align_parts_job_%A_%a.out
+#SBATCH --array=1-115
+#SBATCH --account=chen_fec176
+
+echo 'indir =' $1
+echo 'sample =' $2
+echo 'reference =' $3
+
+python ~/methylation/align_parts.py -i $1 -s $2 -r $3 -p ${SLURM_ARRAY_TASK_ID}
