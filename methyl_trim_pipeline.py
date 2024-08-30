84890423419a5de4f22258e2774b6e71442c4e85
mehdiborji <borjijiji@gmail.com>
2024-08-29 23:09:04 -0400

    add apapter trimming and split command into lines

--- methyl_trim_pipeline.py
+++ methyl_trim_pipeline.py
@@ -20,6 +20,7 @@ print(f"found {len(parts)} parts")
 parts = methyl_utils.find_sub_fastq_parts(indir, sample)
 
 for p in parts:
+    
     fq1 = f"{indir}/{sample}/split/{sample}_R1.part_{p}_clean.fastq"
     fq2 = f"{indir}/{sample}/split/{sample}_R3.part_{p}_clean.fastq"
     
@@ -29,11 +30,16 @@ for p in parts:
     
     out_json = f"{indir}/{sample}/split/{sample}_part_{p}.json"
     out_html = f"{indir}/{sample}/split/{sample}_part_{p}.html"
-    command = f"/home/meb521/fastp -w 10 -i {fq1} -I {fq2} -o {out_fq1} -O {out_fq2} -j {out_json} -h {out_html}"
+    
+    options = "--adapter_sequence=CTATCTCTTATACACATCTCCAAACC --adapter_sequence_r2=CTGTCTCTTATACACATCTGACGCTG"
+    
+    input_output = f"-i {fq1} -I {fq2} -o {out_fq1} -O {out_fq2} -j {out_json} -h {out_html}"
+    
+    command = f"/home/meb521/fastp -w 10 {options} {input_output}"
     
     print(command)
     
     try:
-        subprocess.call(command, shell=True)
+        subprocess.run(command, shell=True)
     except Exception as e:
         print(f"A {e} occurred")
