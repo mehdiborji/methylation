76557e1ea5408579dbe5617191e52274155987da
mehdiborji <borjijiji@gmail.com>
2024-09-10 08:30:21 -0400

    add stand alone scripts for aggregation because they need very different resources

---
+++ scripts/SLURM_aggregate_quad_parts_CpG.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --mem=2G
+#SBATCH -t 0:30:00
+#SBATCH -p short
+#SBATCH -o aggregate_quad_parts_CpG_job_%A_%a.out
+#SBATCH --array=1-12
+#SBATCH --account=chen_fec176
+
+echo 'indir =' $1
+echo 'sample =' $2
+
+python ~/methylation/aggregate_quad_parts.py -i $1 -s $2 -m CpG_context -p ${SLURM_ARRAY_TASK_ID}
