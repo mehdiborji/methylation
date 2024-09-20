556ef71a80a1a7b16065913cec58f97f4365a1b9
mehdiborji <borjijiji@gmail.com>
2024-09-19 22:26:41 -0400

    tag bam with genome version keeping all reads

--- scripts/SLURM_tag_bam_parts.sh
+++ scripts/SLURM_tag_bam_parts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 20
-#SBATCH --mem=2G
-#SBATCH -t 1:00:00
+#SBATCH --mem=8G
+#SBATCH -t 1:40:00
 #SBATCH -p short
 #SBATCH -o tag_bam_parts_job_%A.out
 #SBATCH --account=chen_fec176
@@ -9,5 +9,5 @@
 echo 'indir =' $1
 echo 'sample =' $2
 
-python -u ~/methylation/tag_bam_parts.py -c 20 -i $1 -s $2
+python -u ~/methylation/tag_bam_parts.py -c 20 -i $1 -s $2 --genomic
 #--limit
--- tag_bam_parts.py
+++ tag_bam_parts.py
@@ -6,17 +6,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--cores", type=str)
 parser.add_argument("-i", "--indir", type=str)
 parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-g", "--genomic", default=False, action="store_true")
 parser.add_argument("-l", "--limit", default=False, action="store_true")
+
 args = parser.parse_args()
 
 cores = args.cores
 indir = args.indir
 sample = args.sample
+genomic = args.genomic
 limit = args.limit
+
 
 parts = methyl_utils.find_sub_fastq_parts(indir, sample)
 args = [(indir, sample, part, limit) for part in parts]
 pool = Pool(int(cores))
-results = pool.starmap(methyl_utils.tag_bam_with_barcodes, args)
+
+if genomic:
+    results = pool.starmap(methyl_utils.tag_bam_with_all_barcodes, args)
+else:
+    results = pool.starmap(methyl_utils.tag_bam_with_barcodes, args)
+
 pool.close()
 pool.join()
