9664bfb3a7357b369216a3d75883fa4be342a581
mehdiborji <borjijiji@gmail.com>
2024-09-22 15:01:44 -0400

    separate CpG and Non_CpG count generation job script due to massive resource requirement differences

---
+++ scripts/SLURM_make_count_mtx_windows_CpG.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH --mem=4G
+#SBATCH -t 0:40:00
+#SBATCH -p short
+#SBATCH -o make_count_mtx_windows_CpG_job_%A_%a.out
+#SBATCH --array=1-3
+#SBATCH --account=chen_fec176
+
+echo 'indir =' $1
+echo 'sample =' $2
+echo 'window =' $3
+echo 'reference_index =' $4
+
+python ~/methylation/make_count_mtx_windows.py -i $1 -s $2 -w $3 -m CpG_context -r $4 -p ${SLURM_ARRAY_TASK_ID}
