f8c8a62c3b8c6e6b60243029e238af4d7e855975
mehdiborji <borjijiji@gmail.com>
2024-09-29 21:26:46 -0400

    add filter non_conv file check

--- filter_non_conversion.py
+++ filter_non_conversion.py
@@ -19,13 +19,41 @@ os.chdir(indir)
 print("current dir changed to:", os.getcwd())
 # ref_dir = "/n/scratch/users/m/meb521/GRCh38_v44/"
 
-p = str(parts_batch - 1).zfill(3)
+multiplier_per_task = 6
 
+start = (parts_batch - 1) * multiplier_per_task
+end = parts_batch * multiplier_per_task
 
-bam = f"{sample}/split/output_{p}/{sample}_R1.part_{p}_clean_trim_bismark_bt2_pe.bam"
-command = "/home/meb521/Bismark-0.24.2/filter_non_conversion"
-options = "--paired --threshold 5 --consecutive"
+for p in range(start, end):
+    
+    p = str(p).zfill(3)
 
-submit = f"{command} {options} {bam}"
-print(submit)
-subprocess.run(submit, shell=True)
+    bam_suffix = 'clean_trim_bismark_bt2_pe.bam'
+    bam = f"{sample}/split/output_{p}/{sample}_R1.part_{p}_{bam_suffix}"
+    
+    report_suffix = 'clean_trim_bismark_bt2_pe.non-conversion_filtering.txt'
+    report = f"{sample}/split/output_{p}/{sample}_R1.part_{p}_{report_suffix}"
+    
+    command = "/home/meb521/Bismark-0.24.2/filter_non_conversion"
+    options = "--paired --threshold 5 --consecutive"
+    
+    submit = f"{command} {options} {bam}"
+    
+    if os.path.exists(report):
+        file_size = os.path.getsize(report)
+    
+        if file_size == 0:
+            # Run your command if the file size is zero
+            print(f"The file '{report}' is empty. Running the command...")
+            print(submit)
+            subprocess.run(submit, shell=True)
+        else:
+            print(f"The file '{report}' is not empty. Size: {file_size} bytes")
+    else:
+        print(f"The file '{report}' does not exist.")
+        print(submit)
+        subprocess.run(submit, shell=True)
+    
+    
+    
+    
