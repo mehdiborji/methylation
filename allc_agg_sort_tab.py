e688e20363188e83252e442d3c047e6c5372672d
mehdiborji <borjijiji@gmail.com>
2024-11-22 11:28:17 -0500

    make allc file generation parallel for batches of barcodes
    fix old code where only allch was analyzed

--- allc_agg_sort_tab.py
+++ allc_agg_sort_tab.py
@@ -1,19 +1,20 @@
 import argparse
-import pandas as pd
 from multiprocessing import Pool
 import subprocess
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--cores", type=str)
 parser.add_argument("-i", "--indir", type=str)
 parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-p", "--parts_batch", type=int)
 
 args = parser.parse_args()
 
 cores = args.cores
 indir = args.indir
 sample = args.sample
-
+parts_batch = args.parts_batch
 
 def allc_process(indir, sample, bc):
     allcools_dir = f"{indir}/{sample}/allcools"
@@ -21,13 +22,12 @@ def allc_process(indir, sample, bc):
     cg_file = f"{allcools_dir}/{bc}_CpG_context.tsv"
     ch_file = f"{allcools_dir}/{bc}_Non_CpG_context.tsv"
     allc_file = f"{allcools_dir}/{bc}_allc.tsv"
-
+    allc_sorted_file = f"{allcools_dir}/{bc}_allc_sorted.tsv"
+    
     submit = f"cat {cg_file} {ch_file} > {allc_file}"
     subprocess.run(submit, shell=True)
 
-    allc_sorted_file = f"{allcools_dir}/{bc}_allc_sorted.tsv"
-
-    submit = f"sort -k1,1 -k2,2n {ch_file} > {allc_sorted_file}"
+    submit = f"sort -k1,1 -k2,2n {allc_file} > {allc_sorted_file}"
     subprocess.run(submit, shell=True)
 
     submit = f"bgzip -f {allc_sorted_file}"
@@ -36,12 +36,27 @@ def allc_process(indir, sample, bc):
     submit = f"tabix -s1 -b2 -e2 -f {allc_sorted_file}.gz"
     subprocess.run(submit, shell=True)
 
+multiplier_per_task = 4
+    
+start = (parts_batch - 1) * multiplier_per_task
+end = parts_batch * multiplier_per_task
 
-whitelist = pd.read_csv(f"{indir}/{sample}/{sample}_whitelist.csv")
-all_bcs = sorted(whitelist.tenx_whitelist)
-args = [(indir, sample, bc) for bc in all_bcs]
+with open(f"{indir}/{sample}/{sample}_whitelist_batches.json", "r") as file:
+    bc_splits = json.load(file)
+    
+for b in range(start, end):
+    
+    print(b)
+    args = [(indir, sample, bc) for bc in bc_splits[b]]
+    
+    print(args)
+    
+    pool = Pool(int(cores))
+    results = pool.starmap(allc_process, args)
+    pool.close()
+    pool.join()
 
-pool = Pool(int(cores))
-results = pool.starmap(allc_process, args)
-pool.close()
-pool.join()
+    
+    
+    
+
--- scripts/SLURM_allc_process.sh
+++ scripts/SLURM_allc_process.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -c 16
-#SBATCH --mem=10G
-#SBATCH -t 2:40:00
-#SBATCH -p priority
+#SBATCH -c 4
+#SBATCH --mem=4G
+#SBATCH -t 1:40:00
+#SBATCH -p short
 #SBATCH -o allc_agg_sort_job_%A_%a.out
-#SBATCH --account=chen_fec176
+#SBATCH --account=wu_cjw1
 
 echo 'indir =' $1
 echo 'sample =' $2
 
-python ~/methylation/allc_agg_sort_tab.py -c 16 -i $1 -s $2
+python ~/methylation/allc_agg_sort_tab.py -c 4 -i $1 -s $2 -p ${SLURM_ARRAY_TASK_ID}
