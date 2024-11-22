f33d770c6856a90163461bb98173041e84e9a977
mehdiborji <borjijiji@gmail.com>
2024-11-22 09:30:48 -0500

    clean and format

--- allc_agg_sort_tab.py
+++ allc_agg_sort_tab.py
@@ -14,19 +14,19 @@ cores = args.cores
 indir = args.indir
 sample = args.sample
 
+
 def allc_process(indir, sample, bc):
-    
     allcools_dir = f"{indir}/{sample}/allcools"
-    
-    cg_file = f'{allcools_dir}/{bc}_CpG_context.tsv'
-    ch_file = f'{allcools_dir}/{bc}_Non_CpG_context.tsv'
-    allc_file = f'{allcools_dir}/{bc}_allc.tsv'
-    
+
+    cg_file = f"{allcools_dir}/{bc}_CpG_context.tsv"
+    ch_file = f"{allcools_dir}/{bc}_Non_CpG_context.tsv"
+    allc_file = f"{allcools_dir}/{bc}_allc.tsv"
+
     submit = f"cat {cg_file} {ch_file} > {allc_file}"
     subprocess.run(submit, shell=True)
-    
-    allc_sorted_file  = f'{allcools_dir}/{bc}_allc_sorted.tsv'
-    
+
+    allc_sorted_file = f"{allcools_dir}/{bc}_allc_sorted.tsv"
+
     submit = f"sort -k1,1 -k2,2n {ch_file} > {allc_sorted_file}"
     subprocess.run(submit, shell=True)
 
@@ -35,24 +35,8 @@ def allc_process(indir, sample, bc):
 
     submit = f"tabix -s1 -b2 -e2 -f {allc_sorted_file}.gz"
     subprocess.run(submit, shell=True)
-    
-def fix_allc(indir, sample, bc):
-    
-    print(bc)
-    allcools_dir = f"{indir}/{sample}/allcools"
-    
-    allc_sorted_file  = f'{allcools_dir}/{bc}_allc_sorted.tsv.gz'
-    
-    allc_sorted_mod_file  = f'{allcools_dir}/{bc}_allc_sorted_mod.tsv.gz'
-  
-    submit = f"zcat {allc_sorted_file} | sed 's/CH/CA/g' | bgzip > {allc_sorted_mod_file}"
-    
-    subprocess.run(submit, shell=True)
 
-    submit = f"tabix -s1 -b2 -e2 -f {allc_sorted_mod_file}"
-    
-    subprocess.run(submit, shell=True)
-    
+
 whitelist = pd.read_csv(f"{indir}/{sample}/{sample}_whitelist.csv")
 all_bcs = sorted(whitelist.tenx_whitelist)
 args = [(indir, sample, bc) for bc in all_bcs]
